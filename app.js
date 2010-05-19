24ad8dcf766747cc0dd676b88eeecf599d627fb0
lucassp <lucassp@gmail.com>
2010-05-19 16:22:32 +0300

    First Firefox Working Version!!! Yay!

--- app.js
+++ app.js
@@ -3,9 +3,20 @@
  */
 window.onload = init();
 
-function init()
-{
-	alert("it works");
-	alert("new commit");
-	alert("another commit");
+function init() {
+	var data = $.ajax({
+		url: 'http://query.yahooapis.com/v1/public/yql/lucassp/livescore?country=romania',
+		dataType: XML,
+		success: parseXML
+	});
+}
+
+function parseXML(xml) {
+	$(xml).find("tr").each(function() {
+		var coltext = '';
+		$(this).find("td").each(function() {
+			coltext += "<td colspan='"+$(this).attr("colspan")+"' class='"+$(this).attr("class")+"'>"+$(this).text()+"</td>";
+		});
+		$("#scoretable").append("<tr>"+coltext+"</tr>");
+	})
 }
